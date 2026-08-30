cb01d8ba4916e8281ef73f3c92cc3a98807a69d0
gfranca-cpu <geirlanfa@gmail.com>
2026-08-30 21:42:23 +0000

    Corrige import de dp no MainActivity

--- app/src/main/java/com/musicplayer/app/MainActivity.kt
+++ app/src/main/java/com/musicplayer/app/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.musicplayer.app.data.Album
 import com.musicplayer.app.data.Artist
